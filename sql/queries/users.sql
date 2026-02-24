927275c3d3d9dc5c2cb45b84fe485c9a0a5437f7
alexprimak24 <aprimak43@gmail.com>
2026-02-24 19:13:51 +0200

    made migration

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,13 +1,14 @@
 -- name: CreateUser :exec
-INSERT INTO users (id, created_at, updated_at, name)
+INSERT INTO users (id, created_at, updated_at, name, api_key)
 VALUES (
     ?,
     ?,
     ?,
     ?,
+    ?
 );
 --
 
 -- name: GetUser :one
-SELECT * FROM users WHERE id = ?;
+SELECT * FROM users WHERE api_key = ?;
 --
--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
     created_at TEXT NOT NULL,
     updated_at TEXT NOT NULL,
     name TEXT NOT NULL,
+    api_key TEXT UNIQUE NOT NULL
 );
 
 -- +goose Down
